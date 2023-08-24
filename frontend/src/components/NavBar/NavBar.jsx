774becba9841e34557ccef7db1aace40999f53d1
Hayden-git <hayden.garry99@gmail.com>
2023-08-24 19:04:09 -0400

    links to pages using react link to

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -5,14 +5,6 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   const [openMenu, setOpenMenu] = useState(false);
 
-  let Links = [
-    {name:'HOME', link:'/'},
-    {name:'ABOUT', link:'/about'},
-    {name:'CONTACT', link:'/contact'}
-  ];
-
-  // Responsive Design - Tailwind docs: https://tailwindcss.com/docs/responsive-design 
-  // Margins - Tailwind: https://tailwindcss.com/docs/margin
   return (
     <div className='shadow-md w-full'>
       <div className='md:flex items-center justify-between w-full bg-orange-200 py-4 md:px-5 px-7'>
@@ -30,16 +22,19 @@ export default function NavBar() {
           <ion-icon name={openMenu ? 'close':'menu'}></ion-icon>
         </div>
 
-        {/* Page Links */}
-        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-orange-200 left-0 w-full md:w-auto md:pl-0 pl-8 transition-all duration-500 ease-in ${openMenu ? 'top-20 opacity-100 z-40':'top-[-490px] md:opacity-100 z-40 opacity-30 duration-500'}`}>
-          {
-            Links.map((Link) => (
-              <li key={Link.name} className='md:ml-8 text-xl md:my-0 my-4'>
-                <a href={Link.link} className='text-indigo-800 hover:text-orange-500 duration-500'>{Link.name}</a>
-              </li>
-            )) 
-          }
 
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-orange-200 left-0 w-full md:w-auto md:pl-0 pl-8 transition-all duration-500 ease-in ${openMenu ? 'top-20 opacity-100 z-40':'top-[-490px] md:opacity-100 z-40 opacity-30 duration-500'}`}>
+          <li className='md:ml-8 text-2xl md:my-0 my-4 text-indigo-800 hover:text-orange-500 duration-500'>
+            <Link to='/'>Home</Link>
+          </li>
+          <li className='md:ml-8 text-2xl md:my-0 my-4 text-indigo-800 hover:text-orange-500 duration-500'>
+            <Link to='/about'>About</Link>
+          </li>
+          <li className='md:ml-8 text-2xl md:my-0 my-4 text-indigo-800 hover:text-orange-500 duration-500'>
+            <Link to='/contact'>Contact</Link>
+          </li>
+
+          {/* Search Bar */}
           <div className='flex justify-between md:ml-8 md:my-0 my-4 w-full transition-all duration-500 ease-in'>
             <form 
               action=''
