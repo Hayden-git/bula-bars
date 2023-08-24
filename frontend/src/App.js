54246719ecfffdc01eabb7e84f2fa64d4f873b88
Hayden-git <hayden.garry99@gmail.com>
2023-08-24 17:14:53 -0400

    added about and contact pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,8 @@ import {
 import MapPage from './components/MapPage/MapPage';
 import Login from './components/LoginRegister/Login'; 
 import Register from './components/LoginRegister/Register'; 
+import About from './components/AboutPage/About';
+import Contact from './components/ContactPage/Contact';
 
 const App = () => {
   return (
@@ -25,6 +27,8 @@ const App = () => {
               <Route path='/' element={<MapPage />} />
               <Route path='/login' element={<Login />} />
               <Route path='/register' element={<Register />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/contact' element={<Contact />} />
 
             </Routes>
             <Footer />
